802d4f7a36a3b6479ca5ea58ec3787b1b046cb51
Emirhan Doğan <ag054348emirhan@gmail.com>
2023-03-08 16:40:47 +0300

    Create

--- src/MovieApp/MovieApp/urls.py
+++ src/MovieApp/MovieApp/urls.py
@@ -1,4 +1,4 @@
-"""MovieProject URL Configuration
+"""MovieApp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
